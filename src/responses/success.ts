2182ea27c1bfabe0f9d2c809ee569f0f635e80e7
Hibanka <hibankaoff@gmail.com>
2022-01-03 17:24:38 +0200

    add: default success-response params

--- src/responses/success.ts
+++ src/responses/success.ts
@@ -1,7 +1,7 @@
 import { Response } from './response';
 
 export class SuccessResponse extends Response {
-  constructor(status: 200 | 201 | 202 | 203 | 204 | 205 | 206, body: any) {
+  constructor(status: 200 | 201 | 202 | 203 | 204 | 205 | 206 = 200, body: any = 'Ok') {
     super({
       status,
       body,
